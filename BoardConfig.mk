2b6f4d1b1cc855bc65e462b6c75bc0b89c542662
luka177 <lukapanio@gmail.com>
2023-10-13 23:32:43 +0200

    pipa: Set screen density to 400
    Change-Id: I42f376fa092b1380612142b18b5fdb5b7849c910

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ DEVICE_PATH := device/xiaomi/pipa
 BUILD_BROKEN_DUP_RULES := true
 
 # Display
-TARGET_SCREEN_DENSITY := 440
+TARGET_SCREEN_DENSITY := 400
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/pipa.config
