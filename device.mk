5960ff8b9cf74669efb3028e95c677812c02bcb0
Davide Garberi <dade.garberi@gmail.com>
2023-10-24 14:21:58 +0200

    pipa: Add missing handheld core permissions
     * This gets automatically included when using the
       full_base_telephony makefile, while it doesn't
       with just full_base.
    
    Change-Id: Ic01d6fbef500fbc53d6c1e7351d08fe23d4880d5

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     XiaomiPeripheralManager
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.device.rc
