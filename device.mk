3d264024a8be08b68702aec323d3f782bf1396e6
Davide Garberi <dade.garberi@gmail.com>
2023-10-24 14:23:20 +0200

    pipa: Enable freeform windows by default
    Change-Id: Id1ac898bede7b0c26d346af368704eba136d1ed5

--- device.mk
+++ device.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Rootdir
