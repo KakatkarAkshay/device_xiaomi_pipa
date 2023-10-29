7711067215a84cc50529bc6ac263da8b17756af1
Davide Garberi <dade.garberi@gmail.com>
2023-10-29 21:43:05 +0100

    pipa: Fix screen dimensions
    Change-Id: I75e1b3b2b18572d278230b13fca49dbc379f94d8

--- device.mk
+++ device.mk
@@ -14,15 +14,15 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1800
 
 # Camera
 PRODUCT_COPY_FILES += \
