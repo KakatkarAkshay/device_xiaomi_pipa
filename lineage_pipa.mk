DD3Boh/android_device_xiaomi_pipa

treewide: alioth -> pipa
Change-Id: I78e5f5f86b8c21e1990fca04a7a6ba8939863051

lineage_pipa.mk
@@ -9,17 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_pipa
+PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_BRAND := Pad
+PRODUCT_MODEL := Pad 6
+
+PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/pipa/pipa:13/RKQ1.211001.001/V14.0.7.0.TMZCNXM:user/release-keys

KakatkarAkshay/device_xiaomi_pipa

lineage_pipa.mk
@@ -9,17 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_pipa
+PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Pad 6
+
+PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/pipa/pipa:13/RKQ1.211001.001/V14.0.7.0.TMZCNXM:user/release-keys